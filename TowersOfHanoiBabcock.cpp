94b8c9062381eadf982eb4efdc48656441db525f
Tanner Babcock <babkock@protonmail.com>
2022-09-10 20:11:26 +0000

    Starting Towers of Hanoi

---
+++ TowersOfHanoiBabcock.cpp
@@ -0,0 +1,19 @@
+/**********************************
+ * Name: Basic OO and Encapsulation
+ * Author: Tanner Babcock
+ * Created: September 11, 2022
+ * Course: CIS 152 - Data Structures
+ * Version: 1.0
+ * OS: Void GNU/Linux
+ * Complexity: O(1)
+ * IDE: Emacs
+ * Copyright : This is my own original work based
+ * on specifications issued by our instructor.
+ * Academic Honesty: I attest that this is my original
+ * work. I have not used unauthorized source code,
+ * either modified or unmodified. I have not given
+ * other fellow student(s) access to my program.
+***********************************/
+#include <iostream>
+using namespace std;
+
