57157c0fe204078e08a98323608f24eb2ae38122
Tanner Babcock <babkock@protonmail.com>
2022-09-15 20:23:57 +0000

    Finishing Stack application, added CI for examples

--- StackApplicationBabcock.cpp
+++ StackApplicationBabcock.cpp
@@ -1,7 +1,7 @@
 /*****************************************
  * Name: Convert Infix to Postfix Using Stack
  * Author: Tanner Babcock
- * Created: September 14, 2022
+ * Created: September 15, 2022
  * Course: CIS 152 - Data Structures
  * Version: 1.0
  * OS: Void GNU/Linux
@@ -90,10 +90,30 @@ string toPostfix(string s) {
 }
 
 int main(void) {
-    string exp = "a+b*(c-d^e)^(f+g*h)-i";
+    const int NUMBER_EXPS = 6;
 
-    cout << exp << endl;
-    cout << "Converting..." << endl;
-    cout << toPostfix(exp) << endl;
+    string input;
+    string data[] = {
+        "2+3*4",         // 234*+
+        "a*b+5",         // ab*5+
+        "(1+2)*7",       // 12+7*
+        "a*b/c",         // ab*c/
+        "(a/(b-c+d))*(e-a)*c", // abc-d+/ea-*c*
+        "a/b-c+d*e-a*c"  // ab/c-de*+ac*-
+    };
+
+    for (int x = 0; x < NUMBER_EXPS; x++) {
+        cout << "*********************************************" << endl;
+        cout << "Infix " << (x + 1) << ": " << data[x] << endl;
+        cout << "Postfix: " << toPostfix(data[x]) << endl;
+    }
+
+    cout << "*********************************************" << endl;
+    cout << "Enter infix notation with no spaces: ";
+    getline(cin, input);
+    cout << "Infix: " << input << endl;
+    cout << "Postfix: " << toPostfix(input) << endl;
+    cout << "*********************************************" << endl;
+
     return 0;
 }
--- examples/trieExample.cpp
+++ examples/trieExample.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <bits/stdc++.h>
 using namespace std;
 
 const int ALPHABET_SIZE = 26;
