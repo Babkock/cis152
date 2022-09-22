7a5791ca766c3d02f348e325251a48e293e59506
Tanner Babcock <babkock@protonmail.com>
2022-09-22 20:19:10 +0000

    Starting Module 5 assignments

---
+++ arrayLinkedListBabcock.cpp
@@ -0,0 +1,24 @@
+/****************************************
+ * Name: Array-Based Linked List Implementation
+ * Author: Tanner Babcock
+ * Created: September 24, 2022
+ * Course: CIS 152 - Data Structures
+ * Version: 1.0
+ * OS: Void GNU/Linux
+ * IDE: Emacs
+ * Copyright : This is my own original work based
+ * on specifications issued by our instructor.
+ * Academic Honesty: I attest that this is my original
+ * work. I have not used unauthorized source code,
+ * either modified or unmodified. I have not given
+ * other fellow student(s) access to my program.
+*****************************************/
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(void) {
+	cout << "hello world" << endl;
+	return 0;
+}
+
