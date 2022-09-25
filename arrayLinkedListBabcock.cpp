0b563594b84cb07a50b1895b3c8791ddd7f5c34b
Tanner Babcock <babkock@protonmail.com>
2022-09-25 20:10:03 +0000

    Working on Linked list

--- arrayLinkedListBabcock.cpp
+++ arrayLinkedListBabcock.cpp
@@ -1,7 +1,7 @@
 /****************************************
  * Name: Array-Based Linked List Implementation
  * Author: Tanner Babcock
- * Created: September 24, 2022
+ * Created: September 25, 2022
  * Course: CIS 152 - Data Structures
  * Version: 1.0
  * OS: Void GNU/Linux
@@ -17,8 +17,127 @@
 #include <cstdio>
 using namespace std;
 
+class ListFullException : public exception {
+private:
+    string message;
+public:
+    ListFullException(void) {
+        message = "List is full";
+    }
+    ListFullException(string m) : message(m) { }
+    string getMessage(void) {
+        return message;
+    }
+};
+
+class ListEmptyException : public exception {
+private:
+    string message;
+public:
+    ListEmptyException(void) {
+        message = "List is empty";
+    }
+    ListEmptyException(string m) : message(m) { }
+    string getMessage(void) {
+        return message;
+    }
+};
+
+class LinkedList {
+private:
+    int size;
+    int maxSize;
+    string *list{nullptr};
+public:
+    LinkedList(void) {
+        maxSize = 5;
+        size = -1;
+        list = new string[maxSize];
+    }
+    LinkedList(int s) {
+        maxSize = s;
+        size = -1;
+        list = new string[maxSize];
+    }
+    LinkedList(int m, int s) {
+        maxSize = m;
+        size = s;
+        list = new string[maxSize];
+    }
+
+    LinkedList *createList(int m, int s) {
+        LinkedList *l = new LinkedList(m, s);
+        return l;
+    }
+
+    bool isFull(void) {
+        return (size == (maxSize - 1));
+    }
+    bool isEmpty(void) {
+        return (size == -1) || (size == 0);
+    }
+    void insert(string e) {
+        if (isFull())
+            throw (ListFullException("The list is full!"));
+        list[++size] = e;
+    }
+    void remove(string e) {
+        if (isEmpty())
+            throw (ListEmptyException("The list is empty!"));
+        for (int x = 0; x < size; x++) {
+            if (list[x] == e) {
+                list[x] = "";
+                size--;
+                break;
+            }
+        }
+    }
+    void replace(string e1, string e2) {
+        if (isEmpty())
+            throw (ListEmptyException("The list is empty!"));
+        for (int x = 0; x < size; x++) {
+            if (list[x] == e1) {
+                list[x] = e2;
+                break;
+            }
+        }
+    }
+    int getSize(void) {
+        return size;
+    }
+    string print(void) {
+        string out = "";
+        for (int x = 0; x < size+1; x++) {
+            out += list[x] + "\n";
+        }
+        return out;
+    }
+};
+
 int main(void) {
-	cout << "hello world" << endl;
-	return 0;
+    LinkedList *one = new LinkedList(10);
+    LinkedList *two = new LinkedList(5);
+    one->insert("Uno");
+    one->insert("Dos");
+    one->insert("Tres");
+    cout << "Printing first list" << endl;
+    cout << one->print() << endl;
+    cout << "Size of first list: " << one->getSize() << endl;
+
+    one->remove("Tres");
+    one->remove("Dos");
+    cout << "Printing first list" << endl;
+    cout << one->print() << endl;
+    cout << "Size of first list: " << one->getSize() << endl;
+    one->remove("Uno");
+
+    try {
+        one->remove("Uno");
+    } catch (ListEmptyException e) {
+        cerr << "List empty error: " << e.getMessage() << endl;
+    }
+    delete one;
+
+    return 0;
 }
 
