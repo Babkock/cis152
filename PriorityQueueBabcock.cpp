e6c6f14fac67d941592d36151e5f35ef70baf14c
Tanner Babcock <babkock@protonmail.com>
2022-10-07 16:13:17 +0000

    Working on Priority Queue

--- PriorityQueueBabcock.cpp
+++ PriorityQueueBabcock.cpp
@@ -1,7 +1,7 @@
 /*****************************************
  * Name: Priority Queue Implementation
  * Author: Tanner Babcock
- * Created: August 26, 2022
+ * Created: October 7, 2022
  * Course: CIS 152 - Data Structures
  * Version: 1.0
  * OS: Void GNU/Linux
@@ -15,8 +15,101 @@
 ******************************************/
 #include <iostream>
 #include <string>
+#include <queue>
 #include "PriorityQueueBabcock.h"
 using namespace std;
+
+node makeNode(int j, char p) {
+	return (node){ .job = j, .priority = p };
+}
+
+PriorityQueue::PriorityQueue(void) {
+	size = 0;
+	t = makeNode(1, 'B');
+}
+
+PriorityQueue::PriorityQueue(string e) {
+	size = 1;
+	t = makeNode(1, 'B');
+	qu.push(e);
+}
+
+PriorityQueue::PriorityQueue(string e, int nj) {
+	size = 1;
+	t = makeNode(nj, 'B');
+	qu.push(e);
+}
+
+PriorityQueue::PriorityQueue(string e, int nj, char np) {
+	size = 1;
+	t = makeNode(nj, np);
+	qu.push(e);
+}
+
+PriorityQueue::PriorityQueue(string e, char np, int nj) {
+	size = 1;
+	t = makeNode(nj, np);
+	qu.push(e);
+}
+
+PriorityQueue::~PriorityQueue(void) {
+	size = 0;
+	t = makeNode(0, 'X');
+}
+
+void PriorityQueue::enqueue(string e) {
+	size++;
+	qu.push(e);
+}
+
+string PriorityQueue::dequeue(void) {
+	size--;
+	string out = qu.front();
+	qu.pop();
+	return out;
+}
+
+void PriorityQueue::setNodeJob(int nj) {
+	t.job = nj;
+}
+
+void PriorityQueue::setNodePriority(char np) {
+	t.priority = np;
+}
+
+int PriorityQueue::getNodeJob(void) {
+	return t.job;
+}
+
+char PriorityQueue::getNodePriority(void) {
+	return t.priority;
+}
+
+bool PriorityQueue::isEmpty(void) {
+	return (qu.empty());
+}
+
+string PriorityQueue::peek(void) {
+	return qu.front();
+}
+
+int PriorityQueue::getSize(void) {
+	return size;
+}
+
+string PriorityQueue::printQueue(void) {
+	if (isEmpty())
+		throw (queueEmptyException("Queue is empty!"));
+	string queueString = "";
+
+	while (!qu.empty()) {
+		queueString += qu.front() + "\n";
+		qu.pop();
+	}
+	return queueString;
+}
+
+queueEmptyException::queueEmptyException(void) { }
 
 int main(void) {
 	return 0;
--- PriorityQueueBabcock.h
+++ PriorityQueueBabcock.h
@@ -1,7 +1,7 @@
 /*****************************************
  * Name: Priority Queue Implementation
  * Author: Tanner Babcock
- * Created: October 6, 2022
+ * Created: October 7, 2022
  * Course: CIS 152 - Data Structures
  * Version: 1.0
  * OS: Void GNU/Linux
@@ -12,11 +12,56 @@
  * work. I have not used unauthorized source code,
  * either modified or unmodified. I have not given
  * other fellow student(s) access to my program.
-****************************************/
-#include <iostream>
-using namespace std;
+******************************************/
+#ifndef QUEUEEMPTYEXCEPTION_H
+#define QUEUEEMPTYEXCEPTION_H
+
+#include <queue>
+#include <string>
+
+typedef struct {
+	int job;
+	char priority;
+} node;
+
+node makeNode(int j, char p);
 
 class PriorityQueue {
-	
+private:
+	std::queue<std::string> qu;
+	node t;
+	int size;
+public:
+	PriorityQueue(void);
+	PriorityQueue(std::string e);
+	PriorityQueue(std::string e, int nj);
+	PriorityQueue(std::string e, int nj, char np);
+	PriorityQueue(std::string e, char np, int nj);
+	~PriorityQueue(void);
+	void enqueue(std::string e);
+	std::string dequeue(void);
+	void setNodeJob(int nj);
+	void setNodePriority(char np);
+	int getNodeJob(void);
+	char getNodePriority(void);
+	bool isEmpty(void);
+	std::string peek(void);
+	int getSize(void);
+	std::string printQueue(void);
 };
+
+class queueEmptyException {
+private:
+	std::string msg;
+public:
+	queueEmptyException(const std::string& m) : msg(m) { }
+	queueEmptyException(void);
+	~queueEmptyException(void) { }
+
+	std::string getMessage(void) const {
+		return (msg);
+	}
+};
+
+#endif
 
