ba6d9c67724832a7f5387c68a37bd1af2fc9c697
Tanner Babcock <babkock@protonmail.com>
2022-10-27 19:47:54 +0000

    Thu Oct 27 07:47:54 PM UTC 2022

--- DecisionTreeBabcock.cpp
+++ DecisionTreeBabcock.cpp
@@ -1,4 +1,4 @@
-/***************************************
+/*****************************************
  * Name: Binary Search Decision Tree
  * Author: Tanner Babcock
  * Created: October 27, 2022
@@ -6,7 +6,13 @@
  * Version: 1.0
  * OS: Void GNU/Linux
  * IDE: Emacs
-***************************************/
+ * Copyright : This is my own original work based
+ * on specifications issued by our instructor.
+ * Academic Honesty: I attest that this is my original
+ * work. I have not used unauthorized source code,
+ * either modified or unmodified. I have not given
+ * other fellow student(s) access to my program.
+******************************************/
 #include <iostream>
 #include <string>
 using namespace std;
